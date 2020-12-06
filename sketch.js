8a8e16a448678bfee764ff318fa45c4af7c62b42
aspect <maahirgariba10@gmail.com>
2020-12-06 15:00:15 +0530

    confirmed

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,fly,platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+	fly=new Bird(200,100);
 
 }
 
@@ -61,4 +62,5 @@ function draw(){
 
     bird.display();
     platform.display();
+	fly.display();
 }
